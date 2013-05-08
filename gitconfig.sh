e301de12b8a15e34a146f7d76b38634532dea193
Matthew Wang <mattwyl@gmail.com>
2013-05-08 13:39:17 +0800

    New alias "git up" for pull --rebase

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,6 +7,7 @@ git config --global alias.info "remote -v"
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.hist "log --format=oneline --graph"
+git config --global alias.up "pull --rebase"
 git config --global core.editor vim
 git config --global color.ui true
 git config --global log.abbrevcommit true
