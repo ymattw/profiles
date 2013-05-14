755055231942e4a7bde85c0082330cb2d865c908
Matthew Wang <mattwyl@gmail.com>
2013-05-14 13:50:30 +0800

    ls and info aliases

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -x
 git config --global alias.st status
 git config --global alias.ci commit
-git config --global alias.info "remote -v"
+git config --global alias.info "remote -v show -n"
 git config --global alias.br branch
 git config --global alias.co checkout
-git config --global alias.hist "log --format=oneline --graph"
+git config --global alias.ls "log --oneline --decorate --graph"
 git config --global alias.up "pull --rebase"
 git config --global core.editor vim
 git config --global color.ui true
