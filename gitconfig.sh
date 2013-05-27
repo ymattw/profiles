e4cf044cb62801485c8dd6d5e205aaa4a914c6ef
Matthew Wang <mattwyl@gmail.com>
2013-05-27 21:20:00 +0800

    New alias tail and pick

--- gitconfig.sh
+++ gitconfig.sh
@@ -6,6 +6,8 @@ git config --global alias.info "remote -v show -n"
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ls "log --oneline --decorate --graph"
+git config --global alias.tail "log --oneline --decorate --graph -10"
+git config --global alias.pick cherry-pick
 git config --global alias.up "pull --rebase"
 git config --global core.editor vim
 git config --global color.ui true
