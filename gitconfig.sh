1db7feeb4b3dfc11ee6aef5dcedc93dee7218fee
Matthew Wang <mattwyl@gmail.com>
2013-06-13 17:15:55 +0800

    Arg check for alias.pp; new "rb" and "pl"

--- gitconfig.sh
+++ gitconfig.sh
@@ -10,13 +10,14 @@ git config --global alias.hist "log --oneline --decorate --graph"
 git config --global alias.tail "log --oneline --decorate --graph -10"
 git config --global alias.loggrep "log --decorate --all-match -i --grep"
 git config --global alias.pick cherry-pick
+git config --global alias.rb "rebase -p"
 git config --global alias.up "pull --rebase"
-git config --global alias.ru "pull --rebase upstream"
+git config --global alias.pl "pull --rebase upstream"
 git config --global alias.pu "push upstream"
 git config --global alias.ff "merge --ff-only"
 git config --global alias.noff "merge --no-ff"
 git config --global alias.pp \
-    '!git_pp() { git pull --rebase $1 $2 && git push $1 $2; }; git_pp'
+    '!git_pp() { git pull --rebase ${1?} ${2?} && git push $1 $2; }; git_pp'
 
 git config --global core.editor vim
 git config --global color.ui true
