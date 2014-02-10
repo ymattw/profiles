e33fc2121c5956cd3160e4a7571678a0e0c064fc
Matthew Wang <mattwyl@gmail.com>
2014-02-10 15:56:13 -0800

    Replace alias 'dt' with 'vi'

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,7 +3,7 @@
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.info "remote -v show -n"
-git config --global alias.dt "difftool -y -x vimdiff"
+git config --global alias.vi "difftool -y -x vimdiff"
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ls "log --decorate --name-status"
