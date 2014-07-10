23cdbb06e1254eb9ecc01f64f00d1ad1ec5294e1
Matthew Wang <mattwyl@gmail.com>
2014-07-10 16:41:04 -0700

    Solarized adjustment

--- deploy-shell.sh
+++ deploy-shell.sh
@@ -6,12 +6,13 @@ SELF_DIR=$(cd $(dirname $0) && pwd) || exit 1
 cd $SELF_DIR
 
 if [[ -n $1 ]]; then
-    echo "Copying zsh profile from $HOME to remote $1..."
-    (cd && rsync -az --exclude '.git' .zsh-completions .zshrc $1:)
+    echo "Copying zsh/bash profile from $HOME to remote $1..."
+    (cd && rsync -az --exclude '.git' .zsh-completions .zshrc .bashrc $1:)
 else
     echo "Initializing zsh-completions to $HOME..."
     [[ -d ~/.zsh-completions ]] || {
         git clone git://github.com/zsh-users/zsh-completions.git ~/.zsh-completions
     }
     cp zshrc ~/.zshrc
+    cp bashrc ~/.bashrc
 fi
