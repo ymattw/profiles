5db2b77c001487af9b58776283c54589dc962dff
Matthew Wang <mattwyl@gmail.com>
2015-09-06 09:31:29 +0800

    Add seil script to config key mapping for tmux

---
+++ tmux-seil.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cli=/Applications/Seil.app/Contents/Library/bin/seil
+
+$cli set enable_capslock 1
+$cli set keycode_capslock 100
+$cli set enable_command_r 1
+$cli set keycode_command_r 116
+$cli set enable_option_r 1
+$cli set keycode_option_r 121
