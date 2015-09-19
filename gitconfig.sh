509c321aca46b5903dee2999a726ffc01d9736bb
Matthew Wang <mattwyl@gmail.com>
2015-09-19 22:21:39 +0800

    Remove git configs never used

--- gitconfig.sh
+++ gitconfig.sh
@@ -12,19 +12,11 @@ git config --global alias.hist "log --oneline --decorate --graph"
 git config --global alias.tail "log --oneline --decorate --graph -10"
 git config --global alias.loggrep "log --decorate --all-match -i --grep"
 git config --global alias.pick cherry-pick
-git config --global alias.rb "rebase -p"
-git config --global alias.up "pull --rebase"
-git config --global alias.pl "pull --rebase upstream"
-git config --global alias.pu "push upstream"
 git config --global alias.ff "merge --ff-only"
 git config --global alias.noff "merge --no-ff"
-git config --global alias.pp \
-        '!fn() {
-            git pull --rebase ${1?} ${2?} && git push $1 $2
-        }; fn'
 git config --global alias.tr \
         '!fn() {
-            b=$(git symbolic-ref HEAD) &&
+            local b=$(git symbolic-ref HEAD) &&
             git branch --set-upstream ${b#refs/heads/} ${1?}
         }; fn'
 
