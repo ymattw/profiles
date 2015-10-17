e4f3ef05529d65ed43f42284f36f252b1d880eeb
Matthew Wang <mattwyl@gmail.com>
2015-10-17 18:46:53 +0800

    Fix shell style

--- tmux-seil.sh
+++ tmux-seil.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-cli=/Applications/Seil.app/Contents/Library/bin/seil
+SEIL=/Applications/Seil.app/Contents/Library/bin/seil
 
-$cli set enable_capslock 1
-$cli set keycode_capslock 100
-$cli set enable_command_r 1
-$cli set keycode_command_r 116
-$cli set enable_option_r 1
-$cli set keycode_option_r 121
+# Map Caps Lock to F8
+$SEIL set enable_capslock 1
+$SEIL set keycode_capslock 100
+
+# Map Right Command to PgUp
+$SEIL set enable_command_r 1
+$SEIL set keycode_command_r 116
+
+# Map Right Option to PgDn
+$SEIL set enable_option_r 1
+$SEIL set keycode_option_r 121
