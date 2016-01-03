bd92029fd9ee2d9d1655c86aa9940fd035a373d5
Matthew Wang <mattwyl@gmail.com>
2016-01-03 19:01:36 +0800

    Less verbose

--- bin/install.sh
+++ bin/install.sh
@@ -6,4 +6,4 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 mkdir -p ~/bin
-rsync -av --exclude install.sh --exclude README.md $SELF_DIR/ ~/bin/
+rsync -a --exclude install.sh --exclude README.md $SELF_DIR/ ~/bin/
--- tmux/install.sh
+++ tmux/install.sh
@@ -6,6 +6,3 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 cp $SELF_DIR/tmux.conf ~/.tmux.conf
-
-echo "Tip: see $SELF_DIR/seil-tmux.sh if you want to re-map Caps Lock to F8" \
-     "and use it for tmux prefix key on OS X"
