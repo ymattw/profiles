e76cfaadede0aa204bedf08fe7dda644df6269fa
Matthew Wang <mattwyl@gmail.com>
2016-03-31 00:07:16 +0800

    Better git alias 'ls' and 'hist'

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -7,8 +7,10 @@ git config --global alias.vi "difftool -y -x vimdiff"
 git config --global alias.mt "mergetool -y --tool vi"
 git config --global alias.br branch
 git config --global alias.co checkout
-git config --global alias.ls "log --decorate --name-status"
-git config --global alias.hist "log --oneline --decorate --graph"
+git config --global alias.ls \
+    "log --name-status --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
+git config --global alias.hist \
+    "log --graph --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
 git config --global alias.tail "log --oneline --decorate --graph -10"
 git config --global alias.loggrep "log --decorate --all-match -i --grep"
 git config --global alias.pick cherry-pick
