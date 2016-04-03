15f1e543874eec42cabaf1e02c79fcb01b15bb17
Matthew Wang <mattwyl@gmail.com>
2016-04-03 11:14:06 -0700

    Update color for git alias 'hist'

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -8,9 +8,9 @@ git config --global alias.mt "mergetool -y --tool vi"
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ls \
-    "log --name-status --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
+    "log --name-status --pretty=format:'%C(yellow)%h%C(red)%d%Creset %s %C(blue)- %an, %ar%Creset'"
 git config --global alias.hist \
-    "log --graph --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
+    "log --graph --pretty=format:'%C(yellow)%h%C(red)%d%Creset %s %C(blue)- %an, %ar%Creset'"
 git config --global alias.tail "log --oneline --decorate --graph -10"
 git config --global alias.loggrep "log --decorate --all-match -i --grep"
 git config --global alias.pick cherry-pick
