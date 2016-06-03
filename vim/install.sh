a70d45f56226ef375ba8a6707b514079dbc6425b
Matthew Wang <mattwyl@gmail.com>
2016-06-03 22:53:23 +0800

    Prompt if lua is not supported on install time

--- vim/install.sh
+++ vim/install.sh
@@ -4,12 +4,19 @@ set -o errexit
 set -o nounset
 
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
+readonly TARGET_PLUG_DIR=~/.vim/autoload/plug.vim
 
-rsync $SELF_DIR/vimrc ~/.vimrc
+cp -p $SELF_DIR/vimrc ~/.vimrc
 
-[[ -f ~/.vim/autoload/plug.vim ]] || {
-    curl -fksSLo ~/.vim/autoload/plug.vim --create-dirs \
+[[ -f $TARGET_PLUG_DIR ]] || {
+    echo "Installing vim-plug to $TARGET_PLUG_DIR"
+    curl -fksSLo $TARGET_PLUG_DIR --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+vim --version | grep -Fq "+lua" || {
+    echo "*** Your vim doesn't support lua, highly recommended to" \
+         "upgrade/rebuild to leverage neocomplete for auto completion" >&2
 }
 
 readonly LOG_FILE="/tmp/vim-plug-install-$$.log"
