3866db1907e6163775835278b728af172a46481f
Matthew Wang <mattwyl@gmail.com>
2017-04-06 08:52:13 +0200

    Update karabiner config dir

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,3 +6,5 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 rsync $SELF_DIR/tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/karabiner
+cp karabiner.json ~/.config/karabiner/karabiner.json
