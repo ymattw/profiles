d319621eb0cf31bbc09e8f52a447713b17241a30
Matt Wang <mattwyl@gmail.com>
2017-09-26 10:38:19 +0000

    Sync latest settings
    - Setup karabiner only on macOS
    - Vimrc update: remove imap jj and update tabedit
    - Shorter hostname in PS1/PROMPT

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,5 +6,8 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 rsync $SELF_DIR/tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/karabiner
-cp karabiner.json ~/.config/karabiner/karabiner.json
+
+if [[ $(uname -s) == Darwin ]]; then
+    mkdir -p ~/.config/karabiner
+    cp karabiner.json ~/.config/karabiner/karabiner.json
+fi
