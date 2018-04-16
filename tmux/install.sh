fdc59cdfecbff8c45d287718e6f1837348019b5b
Matthew Wang <mattwyl@gmail.com>
2018-04-16 12:27:21 +0200

    Move karabiner config out from tmux/ to a separate directory.

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,8 +6,3 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 rsync $SELF_DIR/tmux.conf ~/.tmux.conf
-
-if [[ $(uname -s) == Darwin ]]; then
-    mkdir -p ~/.config/karabiner
-    cp karabiner.json ~/.config/karabiner/karabiner.json
-fi
