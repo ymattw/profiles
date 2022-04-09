3aa68e54b356c5f10d22e8e9a4e972831ac376ba
Matt Wang <ymattw@users.noreply.github.com>
2022-04-09 10:40:42 +0200

    Update mac defaults with new Dock preferences

--- mac-defaults/install.sh
+++ mac-defaults/install.sh
@@ -6,13 +6,12 @@ set -o nounset
 readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 [[ $(uname -s) == Darwin ]] || {
-    echo "Skipped (not on Mac OS X)"
+    echo "Skipped (not on macOS)"
     exit 0
 }
 
-# Show "Quit Finder" Menu Item
-defaults write com.apple.finder QuitMenuItem -bool true && killall Finder
-
+# Finder preferences
+#
 # Show Full Path in Finder Title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
@@ -25,9 +24,21 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Show All File Extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
+# Show "Quit Finder" Menu Item
+defaults write com.apple.finder QuitMenuItem -bool true && killall Finder
+
+# TextEdit preferences
+#
 # Use Plain Text Mode as Default
 defaults write com.apple.TextEdit RichText -int 0
 
+# Dock preferences
+#
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0 && killall Dock
+
+# Misc preferences
+#
 # Make key repeat work in browser
 defaults write -g ApplePressAndHoldEnabled -bool false
 
