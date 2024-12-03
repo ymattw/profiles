8fab22c83b7cee2b108c4fa2b5634b95403861f9
Matt Wang <ymattw@users.noreply.github.com>
2024-12-03 20:06:44 +0100

    Configure bufferline to show tabs instead of buffers

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,10 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
-  opts = {},
+  opts = {
+    options = {
+      mode = "tabs",
+      always_show_bufferline = false,
+    },
+  },
 }
