dd876842ff8d05c73fbfffb0220754b60431c19c
Matt Wang <ymattw@users.noreply.github.com>
2024-12-03 20:06:47 +0100

    Update settings of conform plugin
    Auto formatting on save is not perfect yet, need a key to toggle it
    first.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,7 +4,6 @@ return {
   cmd = { "ConformInfo" },
   keys = {
     {
-      -- Customize or remove this keymap to your liking
       "<leader>F",
       function()
         require("conform").format({ async = true })
@@ -21,15 +20,13 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "isort", "black" },
+      go = { "goimports", "gofmt" },
+      -- python = { "black" },
     },
-    -- Set default options
     default_format_opts = {
       lsp_format = "fallback",
     },
-    -- Set up format-on-save
     format_on_save = { timeout_ms = 500 },
-    -- Customize formatters
     formatters = {},
   },
   init = function()
