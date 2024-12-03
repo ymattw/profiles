bfd701451a65daae7e5b238d165ddee7dc109311
Matt Wang <ymattw@users.noreply.github.com>
2024-12-03 20:06:39 +0100

    Customize statusline (lualine)

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,30 @@
+local function toggle_status()
+  local flags = {
+    not vim.o.ignorecase and "\u{eab1}" or nil,
+    vim.o.paste and "\u{f0ea}" or nil,
+    vim.bo.fileformat == "dos" and "\u{f05b3}" or nil,
+  }
+  return table.concat(
+    vim.tbl_filter(function(flag)
+      return flag
+    end, flags),
+    " "
+  )
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  opts = {
+    options = {
+      component_separators = { left = nil, right = nil },
+    },
+    sections = {
+      lualine_c = {
+        { "filename" },
+        { toggle_status, color = { fg = "#b58900" } },
+      },
+      lualine_x = { "encoding", "filetype" },
+    },
+  },
 }
