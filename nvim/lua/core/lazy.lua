44a5128cccf98a69df9ded2c9fe960967b6eff18
Matt Wang <ymattw@users.noreply.github.com>
2024-12-04 21:26:57 +0100

    Do not need to use $WORK_PROFILE
    Just put niubicompany.lua in plugins/ with content like below.
    
    ```
    return {
      { url = "schema://niubicompany-private-url" },
      { import = "niubicompany.foo" },
      { import = "niubicompany.bar" },
    }
    ```

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -16,19 +16,9 @@ local specs = {
   { import = "plugins" },
 }
 
--- Load the work plugin named after $WORK_PROFILE, if exists
-if vim.env.WORK_PROFILE then
-  local ok, plugins = pcall(require, string.format("plugins.%s", vim.env.WORK_PROFILE))
-  if ok then
-    for _, spec in ipairs(plugins) do
-      table.insert(specs, spec)
-    end
-  end
-end
-
 -- Setup lazy and load plugins
 require("lazy").setup({
-  spec = specs,
+  spec = { import = "plugins" },
   defaults = {
     lazy = false,
     version = "*", -- try installing the latest semver version
