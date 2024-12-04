6991ceb26708d25cff85166f0ea8ef0b36fc3fa8
Matt Wang <ymattw@users.noreply.github.com>
2024-12-04 19:12:14 +0100

    Add minimal completion settings
    Syntax parser is not ready yet.

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,37 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "onsails/lspkind-nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+
+    cmp.setup({
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 50,
+          ellipsis_char = "...",
+          show_labelDetails = true,
+        }),
+      },
+      mapping = {
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept selected item with Enter
+        ["<C-Space>"] = cmp.mapping.complete(),
+      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+      }, {
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
