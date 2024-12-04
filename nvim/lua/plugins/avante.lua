9fb812448c61d90b2fb7d0b725d8aec4e4625077
Matt Wang <ymattw@users.noreply.github.com>
2024-12-04 19:12:18 +0100

    Tidy avante.lua (local ollama config does not work yet)

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,49 +2,32 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false, -- always pull the latest change
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- utils lib
+    "MunifTanjim/nui.nvim", -- UI lib
+    "stevearc/dressing.nvim", -- better UI
+  },
   opts = {
-    -- add any opts here
     provider = "openai",
+    ollama = {
+      endpoint = "http://localhost:11434",
+      model = "codegemma",
+      temperature = 0, -- stable response
+      max_tokens = 4096,
+      ["local"] = true,
+    },
+    -- Requires $OPENAI_API_KEY
     openai = {
       model = "gpt-4o-mini",
     },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
+
+    -- TODO: map a key to toggle this?
+    dual_boost = {
+      enabled = false,
+      first_provider = "ollama",
+      second_provider = "openai",
+      timeout = 60000, -- milliseconds
     },
   },
 }
