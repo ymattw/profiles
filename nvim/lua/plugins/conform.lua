da33a2cdd591864dbc3a240b381388415260d184
Matt Wang <ymattw@users.noreply.github.com>
2024-12-05 17:43:20 +0100

    Better format_on_save with <leader>uF to tigger ToggleFormatOnSave

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,6 +1,16 @@
+local filetype_formatters = {
+  go = { "goimports", "gofmt" },
+  lua = { "stylua" },
+  python = { "black" },
+}
+
+local filetype_format_on_save_denylist = {
+  python = true,
+}
+
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
+  event = { "InsertEnter", "BufWritePre" },
   cmd = { "ConformInfo" },
   keys = {
     {
@@ -13,24 +23,40 @@ return {
     },
   },
 
-  -- This will provide type hinting with LuaLS
   ---@module "conform"
   ---@type conform.setupOpts
   opts = {
     -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      go = { "goimports", "gofmt" },
-      -- python = { "black" },
-    },
+    formatters_by_ft = filetype_formatters,
     default_format_opts = {
       lsp_format = "fallback",
     },
-    format_on_save = { timeout_ms = 500 },
+    format_on_save = function(bufnr)
+      if vim.b.format_on_save then
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end
+    end,
+
     formatters = {},
   },
+
   init = function()
-    -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+
+  config = function(_, opts)
+    require("conform").setup(opts)
+
+    -- Initial value
+    vim.b.format_on_save = not filetype_format_on_save_denylist[vim.bo.filetype]
+
+    -- Create a new command ToggleFormatOnSave
+    vim.api.nvim_create_user_command("ToggleFormatOnSave", function(args)
+      vim.b.format_on_save = not vim.b.format_on_save
+      vim.notify(string.format("Format on save: %s", tostring(vim.b.format_on_save)), vim.log.levels.WARN)
+    end, { desc = "Toggle format on save" })
+
+    -- <leader>uF to trigger ToggleFormatOnSave
+    vim.keymap.set("n", "<leader>uF", ":ToggleFormatOnSave<CR>", { desc = "Toggle format on save", noremap = true })
   end,
 }
