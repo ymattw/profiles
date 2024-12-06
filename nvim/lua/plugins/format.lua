4d67287067c1b0fa4e86e16657dff81d6d16ad79
Matt Wang <ymattw@users.noreply.github.com>
2024-12-06 20:54:33 +0100

    Unset formatexpr, otherwise break text paragrah formatting

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -42,10 +42,6 @@ return {
     formatters = formatters,
   },
 
-  init = function()
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
-
   config = function(_, opts)
     require("conform").setup(opts)
 
