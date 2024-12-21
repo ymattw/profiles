5519546d32413e1894e473d5b2c00e977efaecdf
Matt Wang <ymattw@users.noreply.github.com>
2024-12-21 10:19:48 +0100

    Lighter Pmenu background color

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -46,7 +46,7 @@ return {
     hl(0, "IncSearch", { fg = c.base02, bg = c.orange })
     hl(0, "MatchParen", { fg = c.base2 })
     hl(0, "NormalFloat", { link = "Pmenu" })
-    hl(0, "Pmenu", { fg = c.base1, bg = c.base02 })
+    hl(0, "Pmenu", { fg = c.base1, bg = e.lighten(c.base02, 10) })
     hl(0, "Search", { fg = c.base02, bg = c.yellow })
     hl(0, "TabLineSel", { fg = c.base2, bg = c.orange })
 
