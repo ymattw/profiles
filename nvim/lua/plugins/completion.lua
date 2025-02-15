aacb8d5e492eac5c67cbca6b8d99c8813a360d98
Matt Wang <ymattw@users.noreply.github.com>
2025-02-15 19:57:58 +0100

    Add crazyhulk/cmp-sign and enable ghost_text

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -11,6 +11,7 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "onsails/lspkind-nvim",
+      "crazyhulk/cmp-sign",
     },
     config = function()
       local cmp = require("cmp")
@@ -31,15 +32,19 @@ return {
           }),
         },
         mapping = {
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
         },
+        experimental = {
+          ghost_text = true,
+        },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "nvim_cmp_sign" },
           { name = "luasnip" },
           { name = "buffer" },
           { name = "path" },
