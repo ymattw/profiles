128b5a490c65081064e264b89b5cf465767c6431
Matt Wang <ymattw@users.noreply.github.com>
2026-06-05 15:46:57 +0200

    Remove treesitter
    No longer needed in newer neovim, and it's buggy.

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -1,24 +1,4 @@
 return {
-  -- syntax parser
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- :TSInstallInfo to check the full list
-        ensure_installed = {
-          "go",
-          "make",
-          "markdown",
-          "python",
-          "toml",
-        },
-        highlight = {
-          enable = true,
-        },
-      })
-    end,
-  },
-
   -- highlighting other uses of the word under the cursor
   {
     "RRethy/vim-illuminate",
@@ -26,7 +6,7 @@ return {
       require("illuminate").configure({
         delay = 100, -- milliseconds
         filetypes_denylist = { "NvimTree", "TelescopePrompt" },
-        providers = { "treesitter", "lsp", "regex" },
+        providers = { "lsp", "regex" },
         under_cursor = false,
       })
       -- NOTE: colors are dfined in colorscheme.lua
