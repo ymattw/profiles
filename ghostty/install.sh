a10d7ad327368767cf7313eac2045069275fe6d5
Matt Wang <ymattw@users.noreply.github.com>
2026-07-07 12:08:31 +0200

    Disable alternative screen (for ghostty)

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -12,3 +12,9 @@ readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 mkdir -p ~/.config/ghostty
 cp -a $SELF_DIR/config ~/.config/ghostty/
+
+# Ghostty does not currently have a built-in configuration file option to
+# disable the alternate screen buffer. However, you can disable it globally at
+# the system level by stripping the smcup (enter alt screen) and rmcup (exit
+# alt screen) capabilities from Ghostty's terminfo.
+infocmp -1x | grep -vE 'rmcup|smcup' | tic -x -
