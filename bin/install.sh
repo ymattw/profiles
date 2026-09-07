3a2a7093451e9c3dbae8ba5d94b577df8f84e95b
Matt Wang <ymattw@users.noreply.github.com>
2026-09-07 10:36:56 +0200

    Maintain workaround for bypassing ssh wrappers on corp laptop

--- bin/install.sh
+++ bin/install.sh
@@ -7,3 +7,25 @@ readonly SELF_DIR=$(cd $(dirname $0) && pwd)
 
 mkdir -p ~/bin
 rsync -a --exclude install.sh --exclude README.md $SELF_DIR/ ~/bin/
+
+[[ $OSTYPE == darwin* ]] || exit 0
+[[ $(hostname) == *.roam.internal ]] || exit 0
+
+# Workaround to bypass ssh wrappers on corp laptop.
+
+cat > ~/bin/ssh << "EOF"
+#!/bin/bash
+# Use /usr/local/bin/ssh* for cloudtop machines, use defaults otherwise.
+
+if echo "$*" | grep -qE "^w.l|oog..rs.com"; then
+    /usr/local/bin/$(basename $0) -o PubkeyAcceptedAlgorithms=ecdsa-sha2-nistp256-cert-v01@openssh.com "$@"
+else
+    /usr//bin/$(basename $0) -o PubkeyAcceptedAlgorithms=ssh-ed25519 "$@"
+fi
+EOF
+
+chmod +x ~/bin/ssh
+ln -sf ssh ~/bin/scp
+ln -sf ssh ~/bin/sftp
+ln -sf ssh ~/bin/ssh-agent
+ln -sf ssh ~/bin/ssh-keygen
